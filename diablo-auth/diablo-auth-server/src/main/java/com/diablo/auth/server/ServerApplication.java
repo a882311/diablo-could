ed42820c18dcbd5314e665b66cbcec67a2a3ecd3
liumy <liumy@psds.com.cn>
2020-07-23 09:44:30 +0800

    feat:初始提交

---
+++ diablo-auth/diablo-auth-server/src/main/java/com/diablo/auth/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.diablo.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
