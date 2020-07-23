728d3d703de4e885eef66307e76e656ee34b021e
liumy <liumy@psds.com.cn>
2020-07-23 14:12:27 +0800

    feat:添加公共包

---
+++ diablo-common/src/main/java/com/diablo/common/constant/RedisKeyConstants.java
@@ -0,0 +1,17 @@
+package com.diablo.common.constant;
+
+public class RedisKeyConstants {
+
+    public static final String USER_DEPART_PREFIX = "USER:DEPART";
+    public static final String ZUUL_ROUTE_KEY = "ZUUL:ROUTE";
+    public static final String USER_DISABLE = ":dis:";
+    public static final String USER_ABLE = ":able:";
+    public static final String REDIS_USER_PRI_KEY = "AG:AUTH:JWT:PRI";
+    public static final String REDIS_USER_PUB_KEY = "AG:AUTH:JWT:PUB";
+    public static final String REDIS_SERVICE_PRI_KEY = "AG:AUTH:CLIENT:PRI";
+    public static final String REDIS_SERVICE_PUB_KEY = "AG:AUTH:CLIENT:PUB";
+
+    public RedisKeyConstants() {
+    }
+
+}
---
+++ diablo-common/src/test/java/com/diablo/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diablo.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
